32d8bb5f848e9fcdea90a737b0aa9d53f66ec8bf
kajal23-maker <kajalsunhare2311@gmail.com>
2024-11-24 22:50:31 +0530

    Added env variables

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -8,5 +12,6 @@ def home():
     return "<h1>Welcome to Development server!!</h1>"
 
 
+port = int(os.environ.get('FLASK_PORT', 5000))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=port)
